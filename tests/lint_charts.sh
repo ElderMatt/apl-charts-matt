8e9c19e7d4788e7c19f2a897d3d29a0228c0cfc8
ElderMatt <18527012+ElderMatt@users.noreply.github.com>
2026-02-18 13:39:24 +0100

    feat: charts now in subdirectory charts

--- tests/lint_charts.sh
+++ tests/lint_charts.sh
@@ -1,6 +1,6 @@
-for chart in k8s-*; do helm lint $chart $chart --values=tests/test-values.yaml; done;
-helm lint knative-service --values=tests/test-values.yaml
-helm lint postgres-cluster --values=tests/test-values.yaml
-helm lint pgvector-cluster --values=tests/test-values.yaml
-helm lint redis-cluster --values=tests/test-values.yaml
-helm lint rabbitmq-cluster --values=tests/test-values.yaml
+for chart in charts/k8s-*; do helm lint $chart $chart --values=tests/test-values.yaml; done;
+helm lint charts/knative-service --values=tests/test-values.yaml
+helm lint charts/postgres-cluster --values=tests/test-values.yaml
+helm lint charts/pgvector-cluster --values=tests/test-values.yaml
+helm lint charts/redis-cluster --values=tests/test-values.yaml
+helm lint charts/rabbitmq-cluster --values=tests/test-values.yaml
