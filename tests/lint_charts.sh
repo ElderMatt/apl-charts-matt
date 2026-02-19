0db64104f56779016cda9e38887342bd68636205
ElderMatt <18527012+ElderMatt@users.noreply.github.com>
2026-02-19 15:14:12 +0100

    feat: even deeper charts

--- tests/lint_charts.sh
+++ tests/lint_charts.sh
@@ -1,6 +1,6 @@
-for chart in charts/k8s-*; do helm lint $chart $chart --values=tests/test-values.yaml; done;
-helm lint charts/knative-service --values=tests/test-values.yaml
-helm lint charts/postgres-cluster --values=tests/test-values.yaml
-helm lint charts/pgvector-cluster --values=tests/test-values.yaml
-helm lint charts/redis-cluster --values=tests/test-values.yaml
-helm lint charts/rabbitmq-cluster --values=tests/test-values.yaml
+for chart in even/deeper/charts/k8s-*; do helm lint $chart $chart --values=tests/test-values.yaml; done;
+helm lint even/deeper/charts/knative-service --values=tests/test-values.yaml
+helm lint even/deeper/charts/postgres-cluster --values=tests/test-values.yaml
+helm lint even/deeper/charts/pgvector-cluster --values=tests/test-values.yaml
+helm lint even/deeper/charts/redis-cluster --values=tests/test-values.yaml
+helm lint even/deeper/charts/rabbitmq-cluster --values=tests/test-values.yaml
